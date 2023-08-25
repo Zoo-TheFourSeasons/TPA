3d2f2ccd39395878574e323d72d459019e215ca8
zin <zbb@hztrinet.com>
2023-08-25 08:29:09 +0800

    refactor

--- server.pr.py
+++ server.pr.py
@@ -2,20 +2,44 @@
 import argparse
 
 from flask import jsonify, request
+from flask import Flask
+from flask_socketio import SocketIO
 from flask import render_template as rt
 
 from en_decrypt.bp import bp
 from en_decrypt.before import inits
-from app import app_, socket_io
 from cons import APP_ENC as APP
 from base import MetaFile
 import cons
 
+_ = Flask(__name__, template_folder='bs5/templates', static_folder='bs5/static')
+_.debug = True
+_.secret_key = 'x32dc5UTM6eWa8C3qgYRt12u7oiFwSrN'
+_.config.update(
+    WTF_CSRF_SECRET_KEY='x32dc5UTM6eWa8C3qgYRt12u7oiFwSrN',
+    WTF_CSRF_TIME_LIMIT=14400,
+    WTF_CSRF_ENABLED=False,
+    PERMANENT_SESSION_LIFETIME=144000,
+    SESSION_REFRESH_EACH_REQUEST=True,
+    SEND_FILE_MAX_AGE_DEFAULT=3600,
+    FLASK_DB_QUERY_TIMEOUT=0.000001,
+    SCHEDULER_API_ENABLED=True,
+    SCHEDULER_EXECUTORS={"default": {"type": "threadpool", "max_workers": 20}},
+    SCHEDULER_JOB_DEFAULTS={"coalesce": True, "max_instances": 3},
+)
+# init websocket
+socket_io = SocketIO(
+    _,
+    async_mode='threading',
+    ping_timeout=6000,
+    ping_interval=60
+)
 
-@app_.route('/', methods=['get'], defaults={'a': '', 'e': '', 'f': ''}, endpoint='home')
-@app_.route('/webs/<string:f>', defaults={'a': '', 'e': ''}, methods=['get'], endpoint='f')
-@app_.route('/webs/<string:a>/<string:f>', methods=['get'], defaults={'e': ''}, endpoint='a')
-@app_.route('/webs/<string:a>:<string:e>/<string:f>', methods=['get'], endpoint='e')
+
+@_.route('/', methods=['get'], defaults={'a': '', 'e': '', 'f': ''}, endpoint='home')
+@_.route('/webs/<string:f>', defaults={'a': '', 'e': ''}, methods=['get'], endpoint='f')
+@_.route('/webs/<string:a>/<string:f>', methods=['get'], defaults={'e': ''}, endpoint='a')
+@_.route('/webs/<string:a>:<string:e>/<string:f>', methods=['get'], endpoint='e')
 def _(a, e, f):
     _bps = [cons.APP_ENC]
     if not a and not f:
@@ -27,7 +51,7 @@ def _(a, e, f):
     return rt('%s/%s/%s.html' % (a, e, f), **locals())
 
 
-@app_.route('/en_decrypt/index', methods=['get'], endpoint='en_decrypt_index')
+@_.route('/en_decrypt/index', methods=['get'], endpoint='en_decrypt_index')
 def index():
     target = request.args.get('target', '')
     try:
@@ -37,7 +61,7 @@ def index():
     return jsonify(data)
 
 
-@app_.route('/en_decrypt/view', methods=['get'], endpoint='en_decrypt_view')
+@_.route('/en_decrypt/view', methods=['get'], endpoint='en_decrypt_view')
 def view():
     target = request.args.get('target')
     try:
@@ -53,6 +77,6 @@ if __name__ == '__main__':
     parser.add_argument('--port', help='port', type=int, default=80)
 
     ars = parser.parse_args()
-    app_.register_blueprint(bp)
-    app_.before_first_request_funcs.extend(inits)
-    socket_io.run(app_, host=ars.ip, port=int(ars.port), use_reloader=False)
+    _.register_blueprint(bp)
+    _.before_first_request_funcs.extend(inits)
+    socket_io.run(_, host=ars.ip, port=int(ars.port), use_reloader=False)
